40fa34bae13d2e81890c7414561be159eef891cd
Pavel Gyurginchev <gyurginchev@windowslive.com>
2018-12-08 20:53:17 +0200

    Properly render jokes (with <br />)

--- src/FunApp.Models/ViewModels/Home/IndexJokeViewModel.cs
+++ src/FunApp.Models/ViewModels/Home/IndexJokeViewModel.cs
@@ -10,6 +10,8 @@ namespace FunApp.Models.ViewModels.Home
 
         public string Content { get; set; }
 
+        public string HtmlContent => this.Content.Replace("\n", "<br />\n");
+
         public string CategoryName { get; set; }
         public void CreateMappings(IMapperConfigurationExpression configuration)
         {
